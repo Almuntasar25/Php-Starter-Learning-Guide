ffd5a365e22168bd614b9caf2298ae71c9fc883c
almuntasar25 <almuntasar.x@gmail.com>
2025-07-05 21:55:59 +0400

    write echo

--- _starter_files/01_output.php
+++ _starter_files/01_output.php
@@ -1,10 +1,60 @@
 <?php
-// =====================================
-//  joonguini and joe
-// =====================================
-
-/* -------- Output & Comments ------- */
-
 /*
-  A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
-*/
+ * PHP Output and Comments Explanation
+ * This file explains the different output methods in PHP:
+ * - echo
+ * - print
+ * - print_r
+ * - var_dump
+ * - HTML + PHP Integration
+ * - Escaping characters
+ */
+
+//! output -- echo / string - number - html
+
+echo 'hi';  // print hi
+echo 99999; // print hi
+echo '<h1>wellcom</h1>'; // print hi in website
+
+//! 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
